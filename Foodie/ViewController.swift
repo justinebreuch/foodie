7bbfb81100f85a085c61df8edb8232def4453d48
Justine Breuch <justine.breuch@gmail.com>
2015-08-30 00:49:54 -0400

    Initial Commit

---
+++ Foodie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Foodie
+//
+//  Created by Justine Breuch on 8/30/15.
+//  Copyright (c) 2015 Justine Breuch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
